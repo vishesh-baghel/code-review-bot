884232bb7eecfb8bf6b27a4dc80847e178d20faa
vishesh-baghel <visheshbaghel99@gmail.com>
2023-12-14 20:14:17 +0530

    app with basic setup is working

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // Your code here
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
@@ -12,10 +11,4 @@ module.exports = (app) => {
     });
     return context.octokit.issues.createComment(issueComment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
